004d596f7cc4b85adf9d1cc409e9810536a3efea
3moly <3molydev@gmail.com>
2025-11-20 02:44:53 +0400

    - fixed space.kts

--- .space.kts
+++ .space.kts
@@ -19,22 +19,22 @@ job("publish wasm") {
                     mkdir -p ~/.ssh
                     chmod 700 ~/.ssh
                     
-                    echo "${'$'}{'$'}SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
+                    echo "${'$'}SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
                     chmod 600 ~/.ssh/id_ed25519
         
                     eval $(ssh-agent -s)
                     ssh-add ~/.ssh/id_ed25519
                     
-                    echo "Host ${'$'}{'$'}SSH_HOST" >> ~/.ssh/config
+                    echo "Host ${'$'}{SSH_HOST" >> ~/.ssh/config
                     echo "    StrictHostKeyChecking no" >> ~/.ssh/config
                     echo "    UserKnownHostsFile=/dev/null" >> ~/.ssh/config
                     
                     ./gradlew :sample:wasmJsBrowserDistribution
                    
-                    ssh root@${'$'}{'$'}SSH_HOST "mkdir -p /var/www/composedataviz.3moly.com" 
-                    rsync -avz --delete sample/build/dist/wasmJs/productionExecutable/ root@${'$'}{'$'}SSH_HOST:/var/www/composedataviz.3moly.com/
+                    ssh root@${'$'}SSH_HOST "mkdir -p /var/www/composedataviz.3moly.com" 
+                    rsync -avz --delete sample/build/dist/wasmJs/productionExecutable/ root@${'$'}SSH_HOST:/var/www/composedataviz.3moly.com/
                     
-                    curl -F chat_id=253870633 -F text="compose-data-viz build {{ run:number }}" https://api.telegram.org/bot${'$'}{'$'}BOT_TG_TOKEN/sendMessage
+                    curl -F chat_id=253870633 -F text="compose-data-viz build {{ run:number }}" https://api.telegram.org/bot${'$'}BOT_TG_TOKEN/sendMessage
                 """
             }
     }
